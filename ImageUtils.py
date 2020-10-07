024dbc17f5fa5311f7767ef7a343fc8e68d02fb7
avidas <suavidas@gmail.com>
2020-10-07 03:43:51 -0500

    Cleanup

--- ImageUtils.py
+++ ImageUtils.py
@@ -3,6 +3,7 @@ import numpy as np
 from GlobalConstants import IMAGE_HEIGHT, IMAGE_WIDTH
 
 THRESHOLD_VALUE = 200
+THRESHOLD_DELTA = 20
 
 kernel = np.ones((10,10),np.uint8)
 
@@ -18,7 +19,7 @@ def showBrightSpot(img):
     # img = cv2.GaussianBlur(img, (5, 5), 0)
 
     # Adjust threshold to the brightest spot in the frame, so we don't lose track if it flickers
-    THRESHOLD_VALUE = cv2.minMaxLoc(img)[1] - 20
+    THRESHOLD_VALUE = cv2.minMaxLoc(img)[1] - THRESHOLD_DELTA
     img = cv2.threshold(img, THRESHOLD_VALUE, 255, cv2.THRESH_BINARY)[1]
     
     return img
